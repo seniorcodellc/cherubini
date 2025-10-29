9fa2ef02e431d4d65913f07ea15c4e54a76f41e3
LailaRagab <lailaragab144@gmail.com>
2025-10-29 16:10:58 +0300

    Start working on tech dashboard

---
+++ lib/features/tech_dashborad/presentation/screens/tech_dashboard.dart
@@ -0,0 +1,10 @@
+import '../../../../exports.dart';
+
+class TechDashboard extends StatelessWidget {
+  const TechDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
